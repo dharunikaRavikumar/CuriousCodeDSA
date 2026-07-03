4a67ac3d6cdc5b1a02fa3958cf75aff0cabb7e71
dharunikaRavikumar <149648633+dharunikaRavikumar@users.noreply.github.com>
2026-07-03 15:33:32 +0530

    Completed Week02 BasicPattern -Pattern14

---
+++ src/week02/BasicPattern/Pattern14.java
@@ -0,0 +1,23 @@
+package week02.BasicPattern;
+
+import java.util.Scanner;
+
+public class Pattern14 {
+
+	public static void main(String[] args) {
+		Scanner scan=new Scanner(System.in);
+		int n=scan.nextInt();
+		int count=1;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=i;j++) {
+				
+				System.out.print(i);
+				//j=count+1;
+			}
+			System.out.println();
+		}
+		
+
+	}
+
+}
